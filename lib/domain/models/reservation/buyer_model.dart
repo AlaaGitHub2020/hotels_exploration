33db33a9b521760e2005d526d10e66754a517bb7
alaamashkouk <alaamashkouk2@gmail.com>
2024-01-23 12:54:32 +0300

    Fix : Email Validator

--- lib/domain/models/reservation/buyer_model.dart
+++ lib/domain/models/reservation/buyer_model.dart
@@ -25,9 +25,9 @@ class BuyerModel with _$BuyerModel {
 /// Email validator
 extension EmailValidator on String {
   /// Check if email is valid
-  bool isValidEmail() => RegExp(
-        r'^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
-      ).hasMatch(this);
+  bool isValidEmail() =>
+      RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+          .hasMatch(this);
 }
 
 /// Phone validator
